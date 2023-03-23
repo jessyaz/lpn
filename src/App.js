ae921bd1b0e946cfc1864b0957e853cdf35542ff
Jessy Azizi <jessyazizi08@gmail.com>
2023-03-24 01:11:48 +0800

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from "react";
-funtion Test({ test }) {
-  return <div>sasa</div>;
-}
+import logo from './logo.svg';
+import './App.css';
 
-function MyButton() {
+function App() {
   return (
-    <button>I'm a button</button>
-  );
-}
-
-function Greeting({ name }) {
-  return <h1>Hello, {name}</h1>;
-}
-
-export default function App() {
-  return (
-    <>
-      <Greeting name="world" />
-      <MyButton />
-      <Test test="1"/>
-	bababababa
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
