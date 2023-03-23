faf5ad3d670fa5704b160b3cd83cdb64b31951f7
Jessy Azizi <jessyazizi08@gmail.com>
2023-03-24 01:08:00 +0800

    v0.1

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
