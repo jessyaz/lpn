a3c0c437f5b9369bd37dfdc99265d843fe7cf185
Jessy Azizi <jessyazizi08@gmail.com>
2023-03-24 00:50:00 +0800

    version0.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
 function Greeting({ name }) {
   return <h1>Hello, {name}</h1>;
 }
 
 export default function App() {
-  return <Greeting name="world" />
+  return (
+	<Greeting name="world" />
+	<MyButton />
+	);
 }
 
