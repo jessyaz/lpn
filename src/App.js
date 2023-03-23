ed74f2ceebefc3e9abebc51a2daac58513fe3ea4
Jessy Azizi <jessyazizi08@gmail.com>
2023-03-24 01:22:56 +0800

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -21,5 +21,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
